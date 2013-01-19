3cda18f5d7cf6f637b7326961c01dc0368096a7a
hit9 <nz2324@126.com>
2013-01-19 14:09:47 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,24 +1,48 @@
 import os
 
 
+def help():
+    exit("""
+Usage:
+  setup.py vim      to setup .vimrc for vim
+  setup.py          to setup all
+""")
+
 abs_path = os.path.abspath(".")
 
 home_abs_path = os.path.expandvars("$HOME")
 
-dotfiles = [
-    "vim/.vimrc",
-    "vim/.gvimrc",
-    "git/.gitconfig",
-    "tmux/.tmux.conf"
-]
+dct = {
+    "vim": (".vimrc", ),
+    "git": (".gitconfig", ),
+    "tmux": (".tmux.conf", ),
+    "conky": (".conkyrc", ".conky"),
+}
 
-for src in dotfiles:
-    src_abs = os.path.join(abs_path, src)
-    des_abs = os.path.join(home_abs_path, os.path.basename(src))
-    try:
-        os.symlink(src_abs, des_abs)
-    except OSError,e:
-        print "'"+des_abs+"' already exists."
-        raise e
 
-print "Done."
+def install_one(k):
+    print "Install item: ", k
+    v = dct[k]
+    for i in v:
+        src_abs = os.path.join(abs_path, os.path.join(k, i))
+        des_abs = os.path.join(home_abs_path, i)
+        try:
+            print " Create Symbol Link from ", src_abs, " to ", des_abs, " ..."
+            os.symlink(src_abs, des_abs)
+        except OSError, e:
+            print "'" + des_abs + "' already exists."
+            raise e
+
+import sys
+
+if len(sys.argv) > 2:
+    help()
+
+if len(sys.argv) == 2:
+    k = sys.argv[1]
+    if k not in dct:
+        help()
+    install_one(k)
+else:
+    for i in dct.keys():
+        install_one(i)
