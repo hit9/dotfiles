fc97b25d073bb995b0f1d38951da1ae5225f7c49
hit9 <nz2324@126.com>
2013-01-21 16:51:09 +0800

    last commit for setup.py

--- Install.py
+++ Install.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python
+
 import os
 
 
 def help():
     exit("""
 Usage:
-  setup.py [item(s)] [--force]
+Install.py [item(s)] [--force]
 Usage sample:
-  setup.py vim      to setup .vimrc for vim
-  setup.py          to setup all
-  setup.py conky, vim--force  force install conky, vim
+Install.py  vim                to install .vimrc for vim
+Install.py                     to install all
+Install.py conky, vim --force  to force install conky, vim
 """)
 
 abs_path = os.path.abspath(".")
@@ -28,42 +30,93 @@ force_install = False
 
 fonts_installed = False  # mark if fonts installed
 
-colors_dct = {'red': '\033[91m', 'green': '\033[92m', 'yellow': '\033[93m', 'blue': '\033[94m', 'tail': '\033[0m'}
+powerline_installed = False
+
+colors_dct = {
+    'red': '\033[91m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'blue': '\033[94m',
+    'tail': '\033[0m'
+}
 
 
 def color_output(color, msg):
     print colors_dct[color] + msg + colors_dct['tail']
 
 
+def color_return(color, msg):
+    return colors_dct[color] + msg + colors_dct['tail']
+
+
+def rm_if_exists(path):
+    if os.path.exists(path):
+        x = ''
+        if force_install:
+            x = 'yes'
+        while x != 'yes' and x != 'no':
+            x = raw_input(
+                colors_dct['red'] +
+                "Path " + path +
+                " already exists, remove? (yes/no)" +
+                colors_dct['tail'])
+
+        if x == 'yes':
+            color_output('yellow', 'Remove ' + path + ' ...')
+            try:
+                os.unlink(path)
+            except OSError:
+                try:
+                    import shutil
+                    shutil.rmtree(path)
+                except Exception, e:
+                    raise e
+        else:
+            exit(color_return('red', 'Abort.'))
+
+
 def install_one(k):
 
-    global fonts_installed
+    global fonts_installed, powerline_installed
 
     # install powerline for vim
     if k == "vim":
         try:
             import powerline
         except ImportError:
-            color_output('yellow', "install powerline from https://github.com/Lokaltog/powerline ...")
-            code = os.system("pip install git+git://github.com/Lokaltog/powerline")
+            color_output(
+                'yellow',
+                "pip install git+git://github.com/Lokaltog/powerline"
+            )
+            code = os.system(
+                "pip install git+git://github.com/Lokaltog/powerline"
+            )
             if code is not 0:
                 color_output("red", "Failed to install powerline.")
                 exit()
-    
+            powerline_installed = True
+
     # install tmux-powerline for tmux
 
     if k == "tmux":
-        color_output('yellow','Clone tmux-powerline from https://github.com/erikw/tmux-powerline ...')
-        if os.path.exists("tmux-powerline"):
-            os.removedirs("tmux-powerline")
+
+        color_output(
+            'yellow',
+            'Clone tmux-powerline...'
+        )
+
+        rm_if_exists("tmux-powerline")
+
         code = os.system("git clone https://github.com/erikw/tmux-powerline")
+
         if code is not 0:
             color_output("red", "Failed to clone tmux-powerline.")
             exit()
+
         tmux_powerline_abs = os.path.join(home_abs_path, "tmux-powerline")
-        if os.path.exists(tmux_powerline_abs):
-            os.remove(tmux_powerline_abs)
+        rm_if_exists(tmux_powerline_abs)
         os.symlink(os.path.abspath("tmux-powerline"), tmux_powerline_abs)
+        powerline_installed = True
 
     if k in ("vim", "tmux", "sakura") and not fonts_installed:
         install_one("fonts")
@@ -75,19 +128,7 @@ def install_one(k):
         des_dir = os.path.dirname(des_abs)
         if not os.path.exists(des_dir):
             os.makedirs(des_dir)
-
-        if os.path.exists(des_abs):
-            if force_install:
-                color_output('yellow', 'Remove ' + des_abs + ' ...')
-                os.remove(des_abs)
-            else:
-                x = ''
-                while x != 'yes' and x != 'no':
-                    x = raw_input(colors_dct['red'] + "File " + des_abs + " already exists, replace? (yes/no)" + colors_dct['tail'])
-                    if x == 'yes':
-                        os.remove(des_abs)
-                    elif x == 'no':
-                        exit(colors_dct['red'] + 'Abort.' + colors_dct['tail'])
+        rm_if_exists(des_abs)
         os.symlink(src_abs, des_abs)
         color_output('green', "Symbol link:" + i + " -> " + des_abs)
 
@@ -95,7 +136,6 @@ def install_one(k):
         print "Clean font cache.."
         os.system("fc-cache -vf " + des_abs)
         fonts_installed = True
-
 
 import sys
 
@@ -120,4 +160,7 @@ else:
     color_output('yellow', "Please read the README under each sub directory.")
 
 if powerline_installed:
-    color_output('yellow', "Please select the font: 'Inconsolata-g for Powerline.otf' for your terminal.")
+    color_output(
+        'yellow',
+        "Please select the font: 'Inconsolata-g for Powerline.otf' for your terminal."
+    )
