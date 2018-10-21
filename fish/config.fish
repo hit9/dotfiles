86045fbe2a37fa9c6e66a1a0081ebb458c21214c
hit9 <hit9@icloud.com>
2018-10-21 20:36:23 +0800

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+# Change prompt symbol to $
+set pure_symbol_prompt "\$"
+
+# Alias
+alias g "git"
+alias gst "git status"
+alias gc "git commit -ev"
+
+# Pyenv init
+status --is-interactive; and . (pyenv init -|psub)
