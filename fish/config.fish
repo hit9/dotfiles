a1b07947541629b69c8b4c2b49f2d775486ed194
hit9 <hit9@icloud.com>
2021-07-17 17:36:22 +0800

    Update file config

--- fish/config.fish
+++ fish/config.fish
@@ -26,3 +26,18 @@ export GOPRIVATE="github.com/yogorobot/*"
 # pyenv init - --no-rehash | source
 alias python python3
 alias pip pip3
+
+# Rewrite prompt for virtualenv
+functions -c fish_prompt _old_fish_prompt
+function fish_prompt
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
+  _old_fish_prompt
+end
+
+# Tell pipenv to manage venv inside project directory
+export PIPENV_VENV_IN_PROJECT=1
+
+# Alias gsed to sed
+alias sed gsed
