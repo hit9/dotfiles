a4fb1718ba4987848e4c64edd556b2c3599b09a5
hit9 <hit9@icloud.com>
2021-07-21 01:05:12 +0800

    Update fish to 3.3

--- fish/config.fish
+++ fish/config.fish
@@ -1,43 +1,56 @@
 # Using fish@2.7.1
 # brew install fish@2.7.1
-set pure_symbol_prompt ":) ->"
+set pure_symbol_prompt "=>"
+set pure_color_virtualenv "magenta"
 
-# Alias
+# Alias git
 alias g "git"
 alias gst "git status"
 alias gc "git commit -ev"
-
 # Use advanced exa
 alias ls exa
-
 # Use bat instead of cat
 alias cat bat
-
 # Use ag instead of ack
 alias ack ag
+# Alias gsed to sed
+alias sed gsed
 
-export GO111MODULE=on
-
-alias nvim ~/.bin/nvim-osx64/bin/nvim
-
-export GOPRIVATE="github.com/yogorobot/*"
-
-# Pyenv init slow alot
-# pyenv init - --no-rehash | source
 alias python python3
 alias pip pip3
+alias nvim ~/.bin/nvim-osx64/bin/nvim
 
-# Rewrite prompt for virtualenv
-functions -c fish_prompt _old_fish_prompt
-function fish_prompt
-  if set -q VIRTUAL_ENV
-    echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-  end
-  _old_fish_prompt
-end
+export GO111MODULE=on
+export GOPRIVATE="github.com/yogorobot/*"
+
 
 # Tell pipenv to manage venv inside project directory
 export PIPENV_VENV_IN_PROJECT=1
 
-# Alias gsed to sed
-alias sed gsed
+# Rewrite Greeting message
+functions -c fish_greeting _old_fish_greeting
+function fish_greeting
+    _old_fish_greeting
+    if type -q fortune and type -q lolcat
+        fortune -s | lolcat
+    end
+end
+
+# Rewrite prompt for pyenv (overriding pure-fish/pure)
+functions -c _pure_prompt_virtualenv _old_pure_prompt_virtualenv
+function _pure_prompt_virtualenv
+    if set -q PYENV_VERSION
+        # https://github.com/pure-fish/pure/blob/master/functions/_pure_prompt_virtualenv.fish
+        echo -n -s (set_color magenta) "(" (basename "$PYENV_VERSION") ")" (set_color normal) ""
+    end
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color magenta) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) ""
+    end
+end
+ 
+# Rewrite function fish_user_key_bindings
+functions -c fish_user_key_bindings _old_fish_user_key_bindings
+function fish_user_key_bindings
+  _old_fish_user_key_bindings
+  bind \cx edit_command_buffer
+end
--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,4 +0,0 @@
-function fish_user_key_bindings
-  fzf_key_bindings
-  bind \cx edit_command_buffer
-end
