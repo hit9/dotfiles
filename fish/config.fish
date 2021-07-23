e3c97e1456327ae47d1385107e3024f7fc655b77
hit9 <hit9@icloud.com>
2021-07-24 00:37:12 +0800

    Using fish directly

--- fish/config.fish
+++ fish/config.fish
@@ -1,31 +1,44 @@
-# Using fish@2.7.1
-# brew install fish@2.7.1
+# https://github.com/pure-fish/pure
 set pure_symbol_prompt "=>"
 set pure_color_virtualenv "magenta"
 
-# Alias git
-alias g "git"
-alias gst "git status"
-alias gc "git commit -ev"
-# Use advanced exa
-alias ls exa
-# Use bat instead of cat
-alias cat bat
-# Use ag instead of ack
-alias ack ag
-# Alias gsed to sed
-alias sed gsed
+# Basic
+set -x SHELL fish
+set -x TERM screen-256color
+set -x EDITOR vim
+set -x LANG en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
 
-alias python python3
-alias pip pip3
-alias nvim ~/.bin/nvim-osx64/bin/nvim
+# Go
+set -x GOPATH $HOME/gopark
+set -x GO111MODULE on
+set -x GOPRIVATE "github.com/yogorobot/*"
 
-export GO111MODULE=on
-export GOPRIVATE="github.com/yogorobot/*"
+# https://github.com/tj/n
+set -x N_PREFIX $HOME/.n
 
+# $PATH
+set -e fish_user_paths
+set -U fish_user_paths \
+        $GOPATH/bin \
+        $HOME/.cargo/bin \
+        $N_PREFIX/bin \
+        /usr/local/lib/ruby/gems/3.0.0/bin \
+        /usr/local/opt/ruby/bin \
+        /Applications/Postgres.app/Contents/Versions/latest/bin/Applications/Postgres.app/Contents/Versions/latest/bin \
+        /usr/local/sbin \
+        /usr/local/bin \
+        /usr/sbin \
+        /usr/bin \
+        /sbin \
+        /bin \
+        $fish_user_paths
 
-# Tell pipenv to manage venv inside project directory
-export PIPENV_VENV_IN_PROJECT=1
+# oo
+source $HOME/github/oo/env.fish
+ 
+# https://github.com/junegunn/fzf
+set -x FZF_DEFAULT_COMMAND 'fd --type f --exclude .git'
 
 # Rewrite Greeting message
 functions -c fish_greeting _old_fish_greeting
@@ -36,21 +49,38 @@ function fish_greeting
     end
 end
 
+# pipenv
+set -x PIPENV_VENV_IN_PROJECT 1
+
 # Rewrite prompt for pyenv (overriding pure-fish/pure)
 functions -c _pure_prompt_virtualenv _old_pure_prompt_virtualenv
 function _pure_prompt_virtualenv
     if set -q PYENV_VERSION
         # https://github.com/pure-fish/pure/blob/master/functions/_pure_prompt_virtualenv.fish
-        echo -n -s (set_color magenta) "(" (basename "$PYENV_VERSION") ")" (set_color normal) ""
+        echo -n -s (set_color magenta) "" (basename "$PYENV_VERSION") "" (set_color normal) ""
     end
     if set -q VIRTUAL_ENV
-        echo -n -s (set_color magenta) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) ""
+        echo -n -s (set_color magenta) "" (basename "$VIRTUAL_ENV") "" (set_color normal) ""
     end
 end
- 
+
+
 # Rewrite function fish_user_key_bindings
 functions -c fish_user_key_bindings _old_fish_user_key_bindings
 function fish_user_key_bindings
   _old_fish_user_key_bindings
   bind \cx edit_command_buffer
 end
+
+# Alias
+alias g "git"
+alias gst "git status"
+alias gc "git commit -ev"
+alias python python3
+alias pip pip3
+alias nvim ~/.bin/nvim-osx64/bin/nvim
+alias ls exa
+alias cat bat
+alias ack ag
+alias sed gsed
+# alias grep ggrep
