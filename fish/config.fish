f022e4f1233e40ced7a5470dcc6b94a066119413
hit9 <hit9@icloud.com>
2023-06-21 15:39:55 +0800

    Update fish config: add dotenv prompt prefix

--- fish/config.fish
+++ fish/config.fish
@@ -1,60 +1,65 @@
 # https://github.com/pure-fish/pure
-
+# Leader symbol for fish prompt
 set pure_symbol_prompt "\$"
 set pure_color_virtualenv "magenta"
+set pure_show_jobs true
 
 # Basic
 set -x SHELL fish
 set -x TERM screen-256color
-set -x EDITOR vim
+set -x EDITOR nvim
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 
 # Go
 set -x GOPATH $HOME/dev/gopark
 set -x GO111MODULE on
-# set -x GOPRIVATE "github.com/yogorobot/*"
 
 # https://github.com/tj/n
+# Where node versions stores.
 set -x N_PREFIX $HOME/.n
 
-# vcpkg
+# vcpkg (c++ package manager)
+# VCPKG_ROOT is the path of vcpkg repo.
 set -x VCPKG_ROOT $HOME/dev/vcpkg
 
-# $PATH
+# $PATH environment variable
 set -e fish_user_paths
 set -gx fish_user_paths \
-        $HOME/.pyenv/shims \
-        $GOPATH/bin \
-        $HOME/.cargo/bin \
-        $HOME/.dotnet/tools \
-        $N_PREFIX/bin \
-        $VCPKG_ROOT \
-        $HOME/dev/src/flutter/bin \
-        /usr/local/lib/ruby/gems/3.4.2/bin \
-        /usr/local/opt/ruby/bin \
-        /usr/local/opt/llvm/bin \
-        /usr/local/sbin \
-        /usr/local/bin \
-        /usr/sbin \
-        /usr/bin \
-        /sbin \
-        /bin \
-        $HOME/.fzf/bin \
-        $fish_user_paths
+    $HOME/.pyenv/shims \
+    $GOPATH/bin \
+    $HOME/.cargo/bin \
+    $HOME/.dotnet/tools \
+    $N_PREFIX/bin \
+    $VCPKG_ROOT \
+    $HOME/dev/src/flutter/bin \
+    /usr/local/lib/ruby/gems/3.4.2/bin \
+    /usr/local/opt/ruby/bin \
+    /usr/local/opt/llvm/bin \
+    /usr/local/sbin \
+    /usr/local/bin \
+    /usr/sbin \
+    /usr/bin \
+    /sbin \
+    /bin \
+    $HOME/.fzf/bin \
+    $fish_user_paths
 
+# Config files directory.
 set -x XDG_CONFIG_HOME $HOME/.config
 
-# oo
+# oo (Go version manager)
 source $HOME/.oo/env.fish
 
 # homebrew
+# disable auto updates.
 set -x HOMEBREW_NO_AUTO_UPDATE 1
 
 # https://github.com/junegunn/fzf
 set -x FZF_DEFAULT_COMMAND 'fd --type f --exclude .git'
 
 # Rewrite Greeting message
+# https://github.com/jaseg/lolcat
 functions -c fish_greeting _old_fish_greeting
 function fish_greeting
     _old_fish_greeting
@@ -62,9 +67,6 @@ function fish_greeting
         fortune -s | lolcat -l
     end
 end
-
-# pipenv
-set -x PIPENV_VENV_IN_PROJECT 1
 
 # Rewrite prompt for pyenv (overriding pure-fish/pure)
 functions -c _pure_prompt_virtualenv _old_pure_prompt_virtualenv
@@ -82,27 +84,38 @@ end
 # Rewrite function fish_user_key_bindings
 functions -c fish_user_key_bindings _old_fish_user_key_bindings
 function fish_user_key_bindings
-  _old_fish_user_key_bindings
-  bind \cx edit_command_buffer  # Ctrl-X
+    _old_fish_user_key_bindings
+    # Ctrl-X to edit command buffer with $EDITOR
+    bind \cx edit_command_buffer
 end
 
 # fish dotenv function
+# Execute `dotenv` will load environment variables from file `.env`.
 function dotenv --description 'Load environment variables from .env file'
-  set -l envfile ".env"
+    set -l envfile ".env"
 
-  if [ (count $argv) -gt 0 ]
-    set envfile $argv[1]
-  end
-
-  if test -e $envfile
-    for line in (cat $envfile | grep -e '[^[:space:]]' | grep -v '^#')
-        set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
+    if [ (count $argv) -gt 0 ]
+        set envfile $argv[1]
     end
-  end
-end
 
-# bat https://github.com/sharkdp/bat
-set -x BAT_THEME zenburn
+    if test -e $envfile
+        # If envfile exists, set env variables one by one.
+        for line in (cat $envfile | grep -e '[^[:space:]]' | grep -v '^#')
+            set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
+        end
+
+        # Rewrite _pure_prompt function
+        if not functions -q _old_pure_prompt
+            functions -c _pure_prompt _old_pure_prompt
+        end
+
+        # Insert dotenv prefix to prompt
+        function _pure_prompt  --inherit-variable envfile --argument-names exit_code
+            echo -n -s (set_color green) "(" $envfile ")" (set_color normal) " "
+            _old_pure_prompt $exit_code
+        end
+    end
+end
 
 # Alias
 alias g "git"
