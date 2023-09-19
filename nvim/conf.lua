2aedee6e8c30f6bb697b7e3671ba7bd51b9eb764
hit9 <hit9@icloud.com>
2023-09-19 19:02:18 +0800

    Update max_filesize for treesitter

--- nvim/conf.lua
+++ nvim/conf.lua
@@ -11,7 +11,7 @@ require('nvim-treesitter.configs').setup({
     additional_vim_regex_highlighting = false,
     -- disable slow treesitter highlight for large files
     disable = function(lang, buf)
-      local max_filesize = 64 * 1024 -- 64KB
+      local max_filesize = 100 * 1024 -- 100KB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
